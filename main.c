05858a3e486de3800e931d833c6656136285ad3b
Mimbsy23 <elviscobright@gmail.com>
2023-11-17 12:01:46 -0800

    main.c

--- main.c
+++ main.c
@@ -1,67 +1,48 @@
 #include "monty.h"
+struct_t bus = {NULL, NULL, NULL, 0};
+
 /**
- *main - entry point
- *@argc: argument count
- *@argv: argument vectore
- *Return: 0;
+ * main - Entry point for the Monty interpreter.
+ *
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ * Return: 0 on success, or EXIT_FAILURE on failure.
  */
+
 int main(int argc, char *argv[])
 {
-	char *buffer = NULL;
-	size_t size = 0;
-	FILE *file = NULL;
-	ssize_t bufsize;
-	unsigned int line_number = 0;
-	stack_t *stack = NULL;
+	ssize_t read_line = 1;
+	MontyNode_t *stack = NULL;
+	char *content;
+	FILE *file;
+	size_t lentt = 0;
+	unsigned int counter = 0;
 
-	store.head = NULL;
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
+	bus.file = file;
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	store.myfile = file;
-	bufsize = getline(&buffer, &size, file);
-	while (bufsize != -1)
+	while (read_line > 0)
 	{
-		line_number++;
-		store.content = buffer;
-		execute_cmd(&stack, line_number, buffer);
-		stack = NULL;
-		bufsize = getline(&buffer, &size, file);
+		content = NULL;
+		read_line = getline(&content, &lentt, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			funcnCaller(content, &stack, counter, file);
+		}
+		free(content);
 	}
-	free_stack();
+	 clearStack(stack);
 	fclose(file);
-	return (0);
-}
-/**
- *_strdup - duplicate a string
- *@str: pointer to a string
- *Return: str1
- */
-char *_strdup(char *str)
-{
-	char *str1;
-	int i, size = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		size++;
-	}
-	size++;
-
-	str1 = malloc(sizeof(char) * size);
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		str1[i] = str[i];
-	}
-	str1[i] = '\0';
-	return (str1);
+return (0);
 }
